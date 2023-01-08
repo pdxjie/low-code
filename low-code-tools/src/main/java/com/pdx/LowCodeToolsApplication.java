781d7267125ced7050f18d3e81460bb81ec9724c
pdx <pdx_jie@163.com>
2023-01-09 01:12:03 +0800

    feat:初始化项目

---
+++ low-code-tools/src/main/java/com/pdx/LowCodeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.pdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowCodeToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowCodeToolsApplication.class, args);
+    }
+
+}
---
+++ low-code-tools/src/test/java/com/pdx/LowCodeToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowCodeToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
