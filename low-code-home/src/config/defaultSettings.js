129f9eb094ce914cc4d164cf99a95533545f9efb
pdx <pdx_jie@163.com>
2023-01-09 13:34:05 +0800

    feat:处理路由

--- low-code-home/src/config/defaultSettings.js
+++ low-code-home/src/config/defaultSettings.js
@@ -13,17 +13,17 @@
  */
 
 export default {
-  navTheme: 'dark', // theme for nav menu
+  navTheme: 'light', // theme for nav menu
   primaryColor: '#1890ff', // '#F5222D', // primary color of ant design
-  layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
+  layout: 'topmenu', // nav menu position: `sidemenu` or `topmenu`
   contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
-  fixedHeader: false, // sticky header
+  fixedHeader: true, // sticky header
   fixSiderbar: false, // sticky siderbar
   colorWeak: false,
   menu: {
     locale: true
   },
-  title: 'Ant Design Pro',
+  title: '代码生成器',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
