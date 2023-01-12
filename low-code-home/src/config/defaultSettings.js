db1782137330ae142c98cd1a90513d0eac9e635c
pdx <pdx_jie@163.com>
2023-01-12 21:54:22 +0800

    feat:修改布局

--- low-code-home/src/config/defaultSettings.js
+++ low-code-home/src/config/defaultSettings.js
@@ -16,7 +16,7 @@ export default {
   navTheme: 'light', // theme for nav menu
   primaryColor: '#1890ff', // '#F5222D', // primary color of ant design
   layout: 'topmenu', // nav menu position: `sidemenu` or `topmenu`
-  contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
+  contentWidth: 'Fixed', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
   fixedHeader: true, // sticky header
   fixSiderbar: false, // sticky siderbar
   colorWeak: false,
